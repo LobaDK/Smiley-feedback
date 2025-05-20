855d3068b42f9792bdb7d249f8b5f82dd1ff4161
Nicklas <gta5.r6s.bf4@gmail.com>
2025-05-20 10:51:20 +0200

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,66 @@
+#include <Arduino.h>
+#include <driver/rtc_io.h>
+
+#define BUTTON_PIN_BITMASK(GPIO) (1ULL << GPIO)
+#define WAKEUP_GPIO_15 GPIO_NUM_15
+#define WAKEUP_GPIO_2 GPIO_NUM_2
+
+uint64_t bitmask = BUTTON_PIN_BITMASK(WAKEUP_GPIO_15) | BUTTON_PIN_BITMASK(WAKEUP_GPIO_2);
+
+RTC_DATA_ATTR int bootCount = 0;
+
+void print_GPIO_wake_up(){
+  int GPIO_reason = esp_sleep_get_ext1_wakeup_status();
+  Serial.print("GPIO that triggered the wake up: GPIO ");
+  Serial.println((log(GPIO_reason))/log(2), 0);
+}
+
+void print_wakeup_reason(){
+  esp_sleep_wakeup_cause_t wakeup_reason;
+
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+
+  switch (wakeup_reason) {
+    case ESP_SLEEP_WAKEUP_EXT0:     
+      Serial.println("Wakeup caused by external signal using RTC_IO");
+      break;
+    case ESP_SLEEP_WAKEUP_EXT1:
+      Serial.println("Wakeup caused by external signal using RTC_CNTL");
+      print_GPIO_wake_up();
+      break;
+    case ESP_SLEEP_WAKEUP_TIMER:
+      Serial.println("Wakeup caused by timer");
+      break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD:
+      Serial.println("Wakeup caused by touchpad");
+      break;
+    case ESP_SLEEP_WAKEUP_ULP:
+      Serial.println("Wakeup caused by ULP program");
+      break;
+    default:
+      Serial.printf("Wakeup was not caused by deep sleep: %d\n", wakeup_reason);
+      break;
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
+  ++bootCount;
+  Serial.println("Boot number: " + String(bootCount));
+
+  print_wakeup_reason();
+  
+  esp_sleep_enable_ext1_wakeup(bitmask, ESP_EXT1_WAKEUP_ANY_HIGH);
+  rtc_gpio_pulldown_en(WAKEUP_GPIO_15);
+  rtc_gpio_pullup_dis(WAKEUP_GPIO_15);
+  rtc_gpio_pulldown_en(WAKEUP_GPIO_2);
+  rtc_gpio_pullup_dis(WAKEUP_GPIO_2);
+
+  Serial.println("Going to sleep now");
+  esp_deep_sleep_start();
+}
+
+void loop() {
+}
